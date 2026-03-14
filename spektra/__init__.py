c973619edadc93d4e2dcf389c3a6def818a8c866
David Landup <david.landup.0@gmail.com>
2026-03-14 22:31:36 +0900

    add imports to top-level __init__ for convenience

--- spektra/__init__.py
+++ spektra/__init__.py
@@ -1,1 +1,11 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
+
+from .themes import (
+    style,
+    style_matplotlib,
+    style_plotly,
+    get_available_themes,
+    get_theme,
+    get_config,
+    get_cmap,
+)
